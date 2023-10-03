9ad8a0fd6cbe2a646760c88c0aa449182a5e4c61
Vishnupalaiyar <144237658+Vishnupalaiyar@users.noreply.github.com>
2023-10-03 14:29:30 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,23 +1,42 @@
-"""
-Write a function called linear_search_product that takes the list of products and a target product 
-name as input. The function should perform a linear search to find the target product in the list and
-return a list of indices of all occurrences of the product if found, or an empty list if the product is not 
-found.
-"""
+class Player(object):
+    def __init__(self, name, age, skills, style=None):
+        self.name = name
+        self.age = age
+        self.skills = skills
+        self.style = style
+
+    def get_player(self):
+        print(self.name,self.age,self.skills,self.style)
 
 
-def linearSearchProduct(productList, targetProduct):
-  indices = [] 
+class Team(object):
+    def __init__(self, name):
+        self.name = name
+        self._players = []
 
-  for index, product in enumerate (productList): 
-    if product == targetProduct:
-     indices.append(index)
+    def add_player(self, obj):
+        if isinstance(obj, Player):
+            self._players.append(obj)
+        else:
+            print("Please provide player object")
 
-  return indices 
+    def get_players(self):
+        for player in self._players:
+            player.get_player()
 
+if __name__ == "__main__":
 
-#Example usage:
-product =["shoes","boot","Loafer","shoes","sandal","shoes"]
-target = "shoes"
-result = linearSearchProduct (product, target)
-print(result)
+    p1 = Player("Mahendra", 46, "Wicket Keeper", "Right-Hand Batsman")
+    p2 = Player("Sachin", 35, "Batsman", "Right-Hand Batsman")
+    p3 = Player("Saurabh", 44, "Batsman", "Left-Hand Batsman")
+    p4 = Player("Zahir", 38, "Bowler", "Medium Pace Bowler")
+    p5 = Player("Yuvraj", 43, "All rounder")
+
+    t = Team("India")
+    t.add_player(p1)
+    t.add_player(p2)
+    t.add_player(p3)
+    t.add_player(p4)
+    t.add_player(p5)
+
+    t.get_players()
